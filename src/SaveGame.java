12a24c8b6741e43fd5e91ad3848440bef2fd97e9
derinerkan <derin24@gmail.com>
2017-05-08 21:11:54 +0300

    Updated SaveGame

--- src/SaveGame.java
+++ src/SaveGame.java
@@ -8,8 +8,8 @@ public class SaveGame
 {
     private Player player;
     private Leaderboard leaderboard;
-    final String pFilename = "player.txt";
-    final String lFilename = "leaderboard.txt";
+    private final String pFilename = "player.txt";
+    private final String lFilename = "leaderboard.txt";
 
     /**
      * Default constructor for the SaveGame. Tries to recall game data from memory,
@@ -34,29 +34,53 @@ public class SaveGame
     }
 
     /**
-     * Saves the game if it can. If it cant you are fucked
+     * Saves the game if it can.
+     * @return true if it can
      */
-    public void saveGame()
+    public boolean saveGame()
     {
         try
         {
             ObjectOutputStream pOutput = new ObjectOutputStream(new FileOutputStream(pFilename));
-            ObjectOutputStream sOutput = new ObjectOutputStream(new FileOutputStream(sFilename));
+            ObjectOutputStream sOutput = new ObjectOutputStream(new FileOutputStream(lFilename));
 
             pOutput.writeObject(player);
-            sOutput.writeObject(store);
+            sOutput.writeObject(leaderboard);
 
             pOutput.close();
             sOutput.close();
+            return true;
         }
         catch(Exception e)
         {
+            File pFile = new File(pFilename);
+            File lFile = new File(lFilename);
+            try
+            {
+                pFile.createNewFile(); //attempt to create a new file, maybe there isn't one
+                lFile.createNewFile(); //ditto
+            }
+            catch(IOException ex){ex.printStackTrace();}
+
             e.printStackTrace();
+            return false;
         }
 
 
     }
+
+    /**
+     * Get the player retrieved from the savegame
+     * @return the player
+     */
+    public Player getPlayer() {return player;}
+
+    /**
+     * Get the leaderboards retrieved from the savegame
+     * @return the leaderboards
+     */
+    public Leaderboard getLeaderboard() {return leaderboard;}
 }
 
 
-}
+
